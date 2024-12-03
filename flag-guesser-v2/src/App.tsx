05df218c72c05b58a2e87242ceffa2a50982fa86
cshoffmann <cstephanhoffmann@gmail.com>
2024-12-03 15:55:46 -0500

    custom font

--- flag-guesser-v2/src/App.tsx
+++ flag-guesser-v2/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <h1 className="text-blue-500 bg-red-600 text-center text-6xl font-bold">
+      <h1 className="text-blue-400 text-center text-6xl font-bold font-dynapuff">
         Flag Guesser
       </h1>
     </div>
